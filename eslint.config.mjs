2ddf8127a403200df3bea277b59eec7db681500a
Kevin Stubblefield <kstubblefield@live.com>
2025-05-18 18:46:16 -0500

    setup tailwind / daisyui / icons

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,8 @@ import antfu from "@antfu/eslint-config";
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
+// TODO: add tailwind plugin
+
 export default withNuxt(
   antfu({
     type: "app",
