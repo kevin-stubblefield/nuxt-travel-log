b1b7df9b320dff06958feec627d4e462351290c1
Kevin Stubblefield <kevin@stubblefield.io>
2025-05-18 19:01:20 -0500

    Tl 11 (#51)
    * setup tailwind / daisyui / icons
    
    * add landing page
    
    ---------
    
    Co-authored-by: Kevin Stubblefield <kstubblefield@live.com>

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl underline font-bold">
-      Hello World
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
