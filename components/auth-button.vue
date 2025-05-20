0db34b645a6b406c1e6c58c5cb990f1fa65efcf5
Kevin Stubblefield <kevin@stubblefield.io>
2025-05-20 15:16:53 -0500

    Tl 16 (#56)

---
+++ components/auth-button.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { loading, signIn } = useAuthStore();
+</script>
+
+<template>
+  <button :disabled="loading" class="btn btn-accent" @click="signIn()">
+    Sign In with Github
+    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
+  </button>
+</template>
