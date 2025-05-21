be6d250feb9df4380eabae1ebe1d54720a14029a
Kevin Stubblefield <kevin@stubblefield.io>
2025-05-21 14:41:33 -0500

    feat: implement location creation (#61)
    Co-authored-by: Kevin Stubblefield <kstubblefield@live.com>

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,4 +1,5 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import z from "zod";
 
 import { user } from "./auth";
 
@@ -12,4 +13,11 @@ export const location = sqliteTable("location", {
   userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
+
+export const InsertLocation = z.object({
+  name: z.string().min(1).max(100),
+  description: z.string().max(1000).optional(),
+  lat: z.coerce.number().min(-90).max(90),
+  long: z.coerce.number().min(-180).max(180),
 });
