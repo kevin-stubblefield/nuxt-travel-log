4453f5ca7b91e29e3eab988c70abd711124bc459
Kevin Stubblefield <kevin@stubblefield.io>
2025-05-21 18:59:50 -0500

    feat: location names and slugs are url friendly and validated to be unique (#62)
    Co-authored-by: Kevin Stubblefield <kstubblefield@live.com>

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,4 +1,4 @@
-import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, real, sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
 import z from "zod";
 
 import { user } from "./auth";
@@ -13,7 +13,9 @@ export const location = sqliteTable("location", {
   userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
-});
+}, t => [
+  unique().on(t.name, t.userId),
+]);
 
 export const InsertLocation = z.object({
   name: z.string().min(1).max(100),
