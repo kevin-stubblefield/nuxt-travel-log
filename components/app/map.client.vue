f89aa51b5c11a658b4df26eb0f3eeaaa3ec32357
Kevin Stubblefield <kevin@stubblefield.io>
2025-05-22 20:53:44 -0500

    feat: implement map markers (#67)
    Co-authored-by: Kevin Stubblefield <kstubblefield@live.com>

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -1,14 +1,39 @@
 <script lang="ts" setup>
 import { CENTER_USA } from "~/lib/constants";
 
+const mapStore = useMapStore();
+
 const colorMode = useColorMode();
 const style = computed(() => colorMode.value === "dark" ? "/styles/dark.json" : "https://tiles.openfreemap.org/styles/liberty");
 const center = CENTER_USA;
 const zoom = 3;
+
+onMounted(() => {
+  mapStore.init();
+});
 </script>
 
 <template>
-  <MglMap :map-style="style" :center="center" :zoom="zoom">
+  <MglMap
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+  >
     <MglNavigationControl />
+    <MglMarker
+      v-for="point in mapStore.mapPoints"
+      :key="point.id"
+      :coordinates="[point.long, point.lat]"
+    >
+      <template #marker>
+        <div class="tooltip tooltip-top" :data-tip="point.label">
+          <Icon
+            name="tabler:map-pin-filled"
+            size="30"
+            class="text-secondary"
+          />
+        </div>
+      </template>
+    </MglMarker>
   </MglMap>
 </template>
