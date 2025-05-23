709384e0774cc2a93f223883ad1cfa70086f014c
Kevin Stubblefield <kstubblefield@live.com>
2025-05-22 20:08:46 -0500

    feat: add map library

---
+++ components/app/map.client.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { CENTER_USA } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() => colorMode.value === "dark" ? "/styles/dark.json" : "https://tiles.openfreemap.org/styles/liberty");
+const center = CENTER_USA;
+const zoom = 3;
+</script>
+
+<template>
+  <MglMap :map-style="style" :center="center" :zoom="zoom">
+    <MglNavigationControl />
+  </MglMap>
+</template>
