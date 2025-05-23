740060f59ef1c3991cf31d86849f846921a48bd0
Kevin Stubblefield <kevin@stubblefield.io>
2025-05-22 20:10:18 -0500

    feat: add map library (#66)
    Co-authored-by: Kevin Stubblefield <kstubblefield@live.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,25 @@ import "./lib/env";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt", "nuxt-csurf"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+    "@vee-validate/nuxt",
+    "nuxt-csurf",
+    "nuxt-maplibre",
+  ],
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [
       tailwindcss(),
     ],
+    optimizeDeps: {
+      include: [
+        "maplibre-gl",
+      ],
+    },
   },
   eslint: {
     config: {
